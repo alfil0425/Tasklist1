9daba65fd75a6dbca9e92d96a07d1bc282a8ca7c
mauricio <mauricio.leyva2020@gmail.com>
2023-10-26 09:05:57 -0500

    Tasklist funciones basicas

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h5>@All Rights Reserved</h5>
+        </div>
+    )
+}
+
+export default Footer
